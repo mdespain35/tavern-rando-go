5d02e751fd2d499004d7b97fe01da159e9ebfbc8
MICHAEL DE SPAIN <mdespain35@gmail.com>
2024-06-04 11:09:26 -0700

    Initial server setup for randomized button handling.

--- tavernrandoapi/server.go
+++ tavernrandoapi/server.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 	"tavernRando/generator"
 )
 
-func main() {
+func randomHandler(w http.ResponseWriter, r *http.Request) {
 	generator.PopulateGlobalVars()
-	player := generator.CreatePlayerCharacter()
+	if r.URL.Path[1:] == "optimized" {
+		generator.Optimized = true
+	}
+	character := generator.CreatePlayerCharacter()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(character)
+}
 
-	fmt.Println(player)
+func main() {
+	//Stuff for testing JSON Conversion
+
+	// generator.PopulateGlobalVars()
+	// player := generator.CreatePlayerCharacter()
+
+	// b, err := json.Marshal(player)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(string(b))
+
+	http.HandleFunc("/chaos", randomHandler)
+	http.HandleFunc("/optimized", randomHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
